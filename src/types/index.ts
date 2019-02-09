f8bea197411951983b29c67f61ce0f7b8ad067d9
elzup <guild0105@gmail.com>
2019-02-09 22:42:52 +0900

    make types

--- src/types/index.ts
+++ src/types/index.ts
@@ -12,3 +12,105 @@ export type ThunkAction = _ThunkAction<
 >
 
 export type System = {}
+
+export type GameProcess = 'progress' | 'finish'
+
+export type StagePosition = {
+  sx: number
+  sy: number
+}
+
+export type CellPosition = {
+  cx: number
+  cy: number
+}
+
+type SpeedType = 'walk' | 'run'
+
+type Wepon = {
+  id: number
+  bulletAmount: number
+  rate: number
+  // power: number
+}
+
+type Bullet = {
+  position: StagePosition
+  velosity: StagePosition
+  velosityRadian: number
+  hp: number
+  pr: number
+}
+
+export type Player = {
+  id: string
+  position: StagePosition
+  cellPosition: CellPosition
+  wepon: {
+    weponId: number
+    amount: number
+    bullets: Bullet[]
+  }
+  vistion: {
+    pr: number
+  }
+  hp: number // 10
+  speedType: SpeedType
+  speed: number
+}
+
+// position: { x, y }
+// position
+export type CellType = 'wall' | 'empty'
+export type Cell = {
+  id: string
+  type: CellType
+  position: CellPosition
+  // playerIds: number[]
+}
+
+export type Stage = {
+  cells: {
+    [cy: number]: {
+      [cx: number]: Cell
+    }
+  }
+}
+
+export type Position = {
+  x: number
+  y: number
+}
+
+export type Control = {
+  moveStick:
+    | {
+        active: true
+        startPosition: Position
+        currentPosition: Position
+        diffPosition: Position
+        speedType: SpeedType
+        radian: number
+      }
+    | { active: false }
+  bulletButton: {
+    active: boolean
+  }
+}
+
+// number, string, boolean, null, undefined
+// Array<number>
+// number[]
+export type GameProgressState = {
+  processType: 'progress'
+  currentPlayerId: number
+  players: Player[]
+  timeLimit: number
+  stage: Stage
+  control: Control
+}
+
+export type GameFinishState = {
+  processType: 'finish'
+  winnerPlayerId: number
+}
