5ae8fd178cac3ef3a18160fe9ce227838d4866c8
elzup <guild0105@gmail.com>
2019-04-20 22:06:52 +0900

    player center position screen

--- src/containers/GamePage/components/GameProgressScreen.tsx
+++ src/containers/GamePage/components/GameProgressScreen.tsx
@@ -3,10 +3,12 @@ import * as React from 'react'
 import { Stage } from 'react-pixi-fiber'
 
 import { Point } from 'pixi.js'
+import config from '../../../config'
 import { Bullet, Player, Stage as GameStage } from '../../../types'
 import BulletLayer from './BulletsLayer'
 import PlayersLayer from './PlayersLayer'
 import StageLayer from './StageLayer'
+const { aspectRate } = config
 
 type Props = {
   stage: GameStage
@@ -15,21 +17,37 @@ type Props = {
 }
 
 const GameProgressScreen = ({ stage, players, bullets }: Props) => {
-  const width = stage.cw * 24
-  const height = stage.ch * 24
   const screenWidth = window.innerWidth
   const screenHeight = window.innerHeight
   // 画面の比率が縦に長過ぎるか？
   // width / height > screenWidth / screenHeight
-  const isHeightOver = width * screenHeight > screenWidth * height
-  // console.log(isHeightOver ? '縦長' : '横長')
-  const scale = isHeightOver ? screenWidth / width : screenHeight / height
 
+  const isHeightOver = screenWidth > aspectRate * screenHeight
+
+  // console.log(isHeightOver ? '縦長' : '横長')
+  const screenSize = isHeightOver
+    ? {
+        width: '100%',
+        height: `${aspectRate * screenHeight}px`,
+      }
+    : {
+        width: '100%',
+        height: '100vh',
+      }
+  const scale = 2
+
+  const player = players[0]
   return (
-    <div style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
+    <div style={{ ...screenSize, overflow: 'hidden' }}>
       <Stage
         width={screenWidth}
         height={screenHeight}
+        position={
+          new Point(
+            -player.position.sx * scale + screenWidth / 2,
+            -player.position.sy * scale + screenHeight / 2
+          )
+        }
         scale={new Point(scale, scale)}
         options={{ backgroundColor: 0x10bb99 }}
       >
